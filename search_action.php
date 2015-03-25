b34bbf1df8d820ba0b5dea1453d9b7bc203e497e
Bender0 <benjamin.a.r.95@gmail.com>
2015-03-25 14:54:01 +0800

    Update and rename searchAction.php to search_action.php

---
+++ search_action.php
@@ -0,0 +1,29 @@
+<?php
+	include 'db_connect.php';
+	$search = $_REQUEST["booknumber"];
+	$result = mysql_query("SELECT * FROM <tableName> WHERE booknumber = '$search' ", $link) or die ("Database error");
+?>
+	
+<p>back to <a href="index.html">Menu</a></p>
+
+<table>
+<?php	
+	while($row = mysql_fetch_array($result, MYSQL_BOTH))
+{ ?>
+	<tr> 
+		<td><?php echo $row['booknumber'];?>	</td>
+		<td><?php echo $row['dateTimeStart'];?>	</td>
+		<td><?php echo $row['dateTimeStop'];?>	</td>
+		<td><?php echo $row['carType'];?>	</td>
+		<td><?php echo $row['FirstName'];?>	</td>
+		<td><?php echo $row['Surname'];?>	</td>
+		<td><?php echo $row['PhoneNumber']?>	</td>
+		<td><?php echo $row['Email]'?>	</td>
+		<td><div align="center"><a href="search_edit.php?user_booknumber=<?php print ($row['booknumber']);?>">edit</a></div></td>
+    <td><div align="center"><a href="search_delete.php?user_booknumber=<?php print ($row['booknumber']);?>">delete </a></div></td>
+		
+	</tr>
+	
+	<?php  // looping close
+    } ?>
+</table>
